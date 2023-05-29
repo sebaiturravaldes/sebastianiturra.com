bd58bf7a247eec36e5889bc63e613a283eeef2c2
Sebastian Iturra <iturra.sebastian@hotmail.com>
2023-05-29 00:02:05 -0400

    wip

--- app/pokemon/page.js
+++ app/pokemon/page.js
@@ -36,16 +36,19 @@ export default function Home() {
                         <iframe className={styles.iframe} src="https://www.youtube.com/embed/kWLfcGAaUKA" title="Sorteamos Eternatus al Abrir la ETB de Darkness Ablaze de Pokémon Sword &amp; Shield" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                         </div>
                         <h1>Reglas del sorteo</h1>
-                        Debido a que estamos comenzando, se nos imposibilita enviar productos fuera de Chile, por ende solo podrán participar personas que vivan dentro de Chile, se espera que esta restricción se quite en el futuro.
-                        A continuación, se presentan las reglas necesarias para participar en el sorteo.
+                        Debido a que estamos comenzando, el despacho para personas fuera de chile saldría excesivamente caro, por ende se nos imposibilita enviar productos fuera del territorio, por lo cuál, solo podrán participar personas que vivan dentro de Chile.
+                        Se espera que esta restricción se elimine en el futuro.
+                        A continuación, se presentan las reglas necesarias para participar en el sorteo:
                         <ul>
                             <li>Despacho disponible solo para personas que viven en Chile 🇨🇱</li>
-                            <li>Estar subscrito al canal <a href="https://www.youtube.com/@sebaiturravaldes" target="_blank">https://www.youtube.com/@sebaiturravaldes</a></li>
+                            <li>Estar suscrito al canal <a href="https://www.youtube.com/@sebaiturravaldes" target="_blank">https://youtube.com/@sebaiturravaldes</a></li>
                             <li>Comentar el video de arriba 👆🏼</li>
-                            <li>Dar like al video 👆🏼</li>
+                            <li>Dar like al video 👍🏼</li>
                         </ul>
                         <h1>Fecha del sorteo</h1>
                         <p>Se realizará en vivo a través de la plataforma Twitch, será el sábado 24 de Julio a las 21:00 horas Chile por el canal 👉🏼 <a href="https://www.twitch.tv/sebaiturravaldes" target="_blank">https://twitch.tv/sebaiturravaldes</a></p>
+                        <h1>Entrega del sorteo</h1>
+                        <p>Después del sorteo, nos pondremos en contacto con el ganador para coordinar la forma y método de despacho para que pueda recibir su premio.</p>
                 </section>
                 </div>
         </Layout>
